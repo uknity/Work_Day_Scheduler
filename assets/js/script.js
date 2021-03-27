936e525c65cd2a6ebdda2b6773ad11851f5687f9
Liz Emerson <uknity@gmail.com>
2021-03-26 23:03:42 -0400

    commit set storage items and get storage partial code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 //Dom variables
 var currentDayEl = $("#currentDay");
 var calendarContainerEl = $(".calendarContainer");
-var timeEl = $(".time");
 var nowHour = moment().format("HH:mm a");
 
 //creates the rows for the planner
@@ -22,6 +21,8 @@ function createRow() {
 
         var textArea = $("<textarea>");
         textArea.addClass("col-12 col-md-9");
+        textAreaId = [i];
+        textArea.attr('id', textAreaId);
         row.append(textArea);
         
         var save = $("<btn>");
@@ -31,19 +32,24 @@ function createRow() {
         save.append("<img id='saveImg' class='icon' src='assets/images/save icon2.png'/>");
         row.append(save); 
 
-        var timeSpan = $("<span>");
-        timeCol.append(timeSpan);
         var indexHour = parseInt([i]) + parseInt(9);
-        console.log(indexHour);
-        var block = moment(indexHour, "HH").format("h:mm a");
-        console.log(block);
-        // var timeText = block.valueOf();
-        // console.log(timeText);
+        var block = moment(indexHour, "H").format("HH:mm a");
         timeCol.append(block);
-
+            if (block < nowHour) {
+                row.addClass("past");
+            } else if (block = nowHour){
+                row.addClass("present");
+            } else {
+                row.addClass("future");
+            }
+        
     };
 }
-
+// ("h:mm a");
+// function rowColors() {
+    
+//     (if block)
+// }
 
 //checks the time every second
 function checkTime() {
@@ -67,56 +73,34 @@ checkTime();
 //calls the function to create the rows in the planner
 createRow();
 
-// $(".saveBtn").on("click", function () {
-//     var hour = $(this).attr("hour");
-//     console.log(hour);
+// $(document).ready(function() {
+    $(".saveBtn").click(function(){
+        var hour = $(this).attr('hour');
+        console.log(hour);
+        var appointment = $('#' + hour);
+        console.log(appointment);
+        var appt = appointment.val();
+        console.log(appt);
+               
+        //puts the key(hour) and its value(event) into local storage
+        localStorage.setItem("hour", hour);
+        localStorage.setItem("appointment", appt);
+    })
+    
+function getAppointments() {
+var hour = localStorage.getItem("hour");
+var appointment = localStorage.getItem("appointment");
+console.log(hour);
+console.log(appointment);
 
-//     //set the appointment var to the value to the hour.value so that it can be stored as a value
-//     var appointment = document.getElementById(hour).value;
-//     console.log(appointment);
-//     //puts the key(hour) and its value(event) into local storage
-//     localStorage.setItem("hour", hour);
-//     localStorage.setItem("appointment", appointment);
-
-// })
-
-// getAppointments();
-
-// function getAppointments() {
-// var hour = localStorage.getItem("hour");
-// var appointment = localStorage.getItem("appointment");
-// console.log(hour);
-// console.log(appointment);
-
-// if (!hour || !appointment) {
-//     return;
-// }
-// }
-
-
-
-
-
+if (!hour || !appointment) {
+    return;
+} else {
+    $(".startBtn").attr("hour", hour) = hour;
+    $('#' + hour) = appointment;
+    
+}
+}
 
 
-
-
-
-
-
-
-
-// $(".saveBtn").on("click", function(){
-//     var hour = $(this).attr("hour");
-//     console.log($("#"+hour).val());
-// })
-
-
-// add eventlistener on the save button.
-// on click on save, call function to 
-// get value from textarea and put it in local storage
-
-// on page load
-// get values from localstorage and put in appropriate textarea
-
-// on page load, get current time and for all time blocks add past, present or future 
+getAppointments();
